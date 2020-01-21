7c9b5b8e532d3071ba5a6da97f64b211c23e2ebe
deadartt <deadartt@gmail.com>
2020-01-21 17:04:24 +0400

    Start

---
+++ TimeStruct/TimeStruct/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write First Time hours and minutes");
+
+            int FirstHours = Convert.ToInt32(Console.ReadLine());
+            int FirstMinutes = Convert.ToInt32(Console.ReadLine());
+            var FirstTime = new Time(FirstHours, FirstMinutes);
+
+            Console.WriteLine($"First Time is {FirstHours} : {FirstMinutes}");
+
+            Console.WriteLine("Write Second Time hours and minutes");
+            int SecondHours = Convert.ToInt32(Console.ReadLine());
+            int SecondMinutes = Convert.ToInt32(Console.ReadLine());
+            var SecondTime = new Time(SecondHours, SecondMinutes);
+
+            Console.WriteLine($"Second Time is {SecondHours} : {SecondMinutes}");
+
+            Console.WriteLine("Choose Operation:\n" +
+                "1: +\n " +
+                "2: -\n " +
+                "3: Implicite From Int to Time \n"+
+                "4: Explicite Time to int\n ");
+
+            int operate = Convert.ToInt32(Console.ReadLine());
+
+            switch(operate)
+            {
+                case 1:
+                    Time ResultOfAdding = FirstTime + SecondTime;
+                    
+                    Console.WriteLine($"New Time is: {ResultOfAdding.ToString()}");
+                    break;
+
+                case 2:
+                    Time ResultOfSubtracting = FirstTime - SecondTime;
+                    Console.WriteLine($"New Time is: {ResultOfSubtracting.ToString()}");
+                    break;
+
+                case 3:
+                   Time ThirdTime = (Time)SecondTime;
+                    Console.WriteLine($"{ThirdTime}");
+                    break;
+
+                case 4:
+                    int Hours = (int)FirstTime;
+                    Console.WriteLine($"{Hours}");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ TimeStruct/TimeStruct/Time.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeStruct
+{
+    public struct Time
+    {
+        public int TimeOfDay { get; set; }
+        public int NumberOfMinutes { get; set; }
+        public int NumberOfHours { get; set; }
+
+        public static Time Noon
+        {
+            get
+            {
+                return new Time(12, 0);
+            }
+        }
+
+        public Time(int hours, int minutes)
+        {
+            if (hours > 23 || hours < 0)
+            {
+                throw new Exception("Wrong hours Format");
+            }
+
+            NumberOfHours = hours;
+
+            if (minutes > 59 || minutes < 0)
+            {
+                throw new Exception("Wrong minutes format");
+            }
+
+            
+            NumberOfMinutes = minutes;
+            TimeOfDay = (60 * hours) + minutes;
+        }
+
+        
+
+        public static Time operator +(Time FirstTime, Time SecondTime)
+        {
+            
+            var ReturnResult = new Time() // getting 3rd Time Type 
+            {
+                NumberOfHours = FirstTime.NumberOfHours + SecondTime.NumberOfHours,
+                NumberOfMinutes = FirstTime.NumberOfMinutes + SecondTime.NumberOfMinutes,
+                TimeOfDay = FirstTime.TimeOfDay + SecondTime.TimeOfDay
+            };
+
+            if (ReturnResult.NumberOfMinutes >= 60)
+            {
+                ++ReturnResult.NumberOfHours;
+                ReturnResult.NumberOfHours -= 60;
+            }
+
+            return ReturnResult;
+
+        }
+
+        public static Time operator -(Time FirstTime, Time SecondTime)
+        {
+            var ReturnResult = new Time() // getting 3rd Time Type 
+            {
+                NumberOfHours = Math.Abs(FirstTime.NumberOfHours - SecondTime.NumberOfHours),
+                NumberOfMinutes =Math.Abs( FirstTime.NumberOfMinutes - SecondTime.NumberOfMinutes),
+
+                TimeOfDay = FirstTime.TimeOfDay - SecondTime.TimeOfDay
+            };
+
+            if (ReturnResult.NumberOfMinutes >= 60)
+            {
+                ++ReturnResult.NumberOfHours;
+                ReturnResult.NumberOfHours -= 60;
+            }
+
+            return ReturnResult;
+
+        }
+
+        public static implicit operator Time(int minutes)
+        {
+            var ResultOfHours = minutes / 60;
+            var ResultOfMinutes = minutes % 60;
+
+            return new Time(ResultOfHours, ResultOfMinutes);
+
+        }
+
+        public static explicit operator int(Time time)
+        {
+            return (time.NumberOfHours * 60) + time.NumberOfMinutes;
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            return $"{NumberOfHours.ToString()} : {NumberOfMinutes.ToString()}";
+        }
+
+
+    }
+}
